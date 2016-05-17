4973d44aad760352d3b3f9f7028df4938b376156
David Yeung <quxbaz@gmail.com>
2016-05-17 15:43:13 -0400

    fireOnce returns the wrapper function

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const fireOnce = (el, event, handler) => {
   /*
-    Fires an handler once, then detaches it.
+    Fires an handler once, then detaches it. Returns the wrapper
+    function so that it can be removed using removeEventListener
   */
-  el.addEventListener(event, function once() {
+  function once() {
     handler.apply(null, arguments)
     el.removeEventListener(event, once)
-  })
+  }
+  el.addEventListener(event, once)
+  return once
 }
 
 export {fireOnce}
--- tests/index.js
+++ tests/index.js
@@ -4,6 +4,7 @@ import {fireOnce} from 'dom-util'
 const doc = document
 
 describe("fireOnce()", () => {
+
   it("triggers an event handler only once.", () => {
     let el = doc.createElement('button')
     let i = 0
@@ -12,6 +13,16 @@ describe("fireOnce()", () => {
     el.click()
     expect(i).toEqual(1)
   })
+
+  it("Returns the wrapper function.", () => {
+    let el = doc.createElement('button')
+    let i = 0
+    const fn = fireOnce(el, 'click', () => i++)
+    el.removeEventListener('click', fn)
+    el.click()
+    expect(i).toEqual(0)
+  })
+
 })
 
 
